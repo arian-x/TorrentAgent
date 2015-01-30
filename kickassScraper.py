e269803f49f1f532d52929ae973cca373860231e
arianx <arian.ah@hotmail.com>
2015-01-30 19:26:55 +0330

    scraper first

--- kickassScraper.py
+++ kickassScraper.py
@@ -2,15 +2,21 @@ __author__ = 'arian'
 from BeautifulSoup import BeautifulSoup
 import urllib
 import urllib2
+import cookielib
 
 
 
 class Scrape:
     def __init__(self,link):
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        opener.add_handler([('User-agent', "Magic Browser")])
+        urllib2.install_opener(opener)
+
         print "sending req"
-        res = urllib2.urlopen(link)
+        res = opener.open(link)
         print "got the response"
-        print res
+        #print res
         soup = BeautifulSoup(res.read())
         names = soup.findAll('a',{'class':'cellMainLink'})
         magnets = soup.findAll('a',{'class':'imagnet icon16 askFeedbackjs'})
