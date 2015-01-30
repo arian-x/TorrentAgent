2b5735fb428ca1210dee556526fe91d1c4a3e2d0
arianx <arian.ah@hotmail.com>
2015-01-30 19:12:39 +0330

    scraper first

--- kickassScraper.py
+++ kickassScraper.py
@@ -5,3 +5,21 @@ import urllib2
 
 
 
+class Scrape:
+    def __init__(self,link):
+        print "sending req"
+        res = urllib.urlopen(link)
+        print "got the response"
+        soup = BeautifulSoup(res)
+        names = soup.findAll('a',{'class':'cellMainLink'})
+        magnets = soup.findAll('a',{'class':'imagnet icon16 askFeedbackjs'})
+        print names
+        print magnets
+
+
+
+
+
+
+scp = Scrape("https://kickass.so/search/1080p%20category:xxx/")
+
