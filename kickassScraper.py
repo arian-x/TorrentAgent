e9ccda6f02c1c127ba850a1695e4bfc715037045
arianx <arian.ah@hotmail.com>
2015-02-02 13:14:40 +0330

    scraper first

--- kickassScraper.py
+++ kickassScraper.py
@@ -1,27 +1,31 @@
 __author__ = 'arian'
-from BeautifulSoup import BeautifulSoup
-import urllib
-import urllib2
-import cookielib
-
+#from BeautifulSoup import BeautifulSoup
+#import urllib
+#import urllib2
+#import cookielib
+from urllib.request import urlopen
 
 
 class Scrape:
     def __init__(self,link):
-        cj = cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-        opener.add_handler([('User-agent', "Magic Browser")])
-        urllib2.install_opener(opener)
+        #cj = cookielib.CookieJar()
+        #opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        #opener.add_handler([('User-agent', "Magic Browser")])
+        #urllib2.install_opener(opener)
 
-        print "sending req"
-        res = opener.open(link)
-        print "got the response"
+        print("sending req")
+        #res = opener.open(link)
+#        res = urllib.request.urlopen("https://kickass.so/search/1080p%20category:xxx/")
+
+        html = urlopen("https://kickass.so/search/1080p%20category:xxx/")
+        print(html.read())        
+#        print("got the response")
         #print res
-        soup = BeautifulSoup(res.read())
-        names = soup.findAll('a',{'class':'cellMainLink'})
-        magnets = soup.findAll('a',{'class':'imagnet icon16 askFeedbackjs'})
-        print names
-        print magnets
+        #soup = BeautifulSoup(res.read())
+        #names = soup.findAll('a',{'class':'cellMainLink'})
+        #magnets = soup.findAll('a',{'class':'imagnet icon16 askFeedbackjs'})
+        #print names
+        #print magnets
 
 
 
