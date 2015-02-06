def190b60a7c162b0fff03cc70afca1f81e9c686
arianx <arian.ah@hotmail.com>
2015-02-06 10:05:21 +0330

    new api

--- api.py
+++ api.py
@@ -1,18 +1,19 @@
 __author__ = 'arian'
 from flask import Flask,request,url_for,redirect
-from flask.ext.socketio import SocketIO
+#from flask.ext.socketio import SocketIO
 
 import kickassScraper
 
 import os
-app = Flask(__name__,static_url_path=os.getcwd())
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app = Flask(__name__,static_url_path=os.getcwd()+'/')
+#app.config['SECRET_KEY'] = 'secret!'
+#socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     #return redirect(url_for('static', filename='index.html'))
     return app.send_static_file("index.html")
+
 @app.route("/download")
 def download():
     magnet = request.args.get("mag")
@@ -26,4 +27,5 @@ def scrape():
 
 
 if __name__ == "__main__":
-    socketio.run(app)
+    #socketio.run(app)
+    app.run()
